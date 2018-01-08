ac5db647b6e92335dce993dce6ebdbbc0382dc15
Kyriakos Kentzoglanakis <kyriakos.kentzoglanakis@gmail.com>
2018-01-08 23:57:12 +0200

    Adds logrus

--- project.go
+++ project.go
@@ -1,18 +1,21 @@
 package main
 
 import (
-	"log"
+	"fmt"
+	"os"
 
 	"github.com/fsnotify/fsnotify"
+
+	log "github.com/sirupsen/logrus"
 )
 
 type Project struct {
 	root    string   // project filesystem root (absolute)
 	args    string   // args to ctags binary
+	include []string // directories within root to monitor
 	exclude []string // directories to exclude from tagging
+	// TODO: add file types (a regex??)
 }
-
-// need to recursively figure out all project directories
 
 func (project *Project) Monitor(watcher *fsnotify.Watcher) {
 	var err error
@@ -29,12 +32,25 @@ func (project *Project) Monitor(watcher *fsnotify.Watcher) {
 	for {
 		select {
 		case event := <-watcher.Events:
-			log.Println("event:", event)
-			if event.Op&fsnotify.Write == fsnotify.Write {
-				log.Println("modified file:", event.Name)
+			// check event type: Create, Write, Remove, Rename
+			log.Info(event)
+			// if event type: remove / rename
+			// ==> if directory: remove from project.include
+			// if event type: create / write
+			// ==> if directory: add to project.include
+			fi, err := os.Stat(event.Name)
+			if err != nil {
+				continue
 			}
+			if fi.IsDir() {
+				// add the directory to project files
+				fmt.Println("[directory]")
+			} else {
+				fmt.Println("[file]")
+			}
+
 		case err = <-watcher.Errors:
-			log.Println("error:", err)
+			log.Error("error:", err)
 		}
 
 	}
